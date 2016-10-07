768ae132b24fbf4c7d80127df23f211b67f59ca9
psi <psi@7io.org>
2016-10-07 21:43:42 +0900

    po

--- validity.v
+++ validity.v
@@ -9,3 +9,45 @@ Lemma it_is_valid_if_D_is_not_empty:
   apply Empty_set_ind.
   contradiction x.
 Qed.
+
+Lemma p1:
+  forall (D:Set) (a:D->Prop) (b:D->Prop),
+  (exists (x:D), a x /\ b x) ->  (exists (x:D), a x) /\   (exists (x:D), b x).
+  intros.
+  destruct H.
+  destruct H as [A B].
+  apply conj.
+  exists x.
+  exact A.
+  exists x.
+  exact B.
+Qed.
+
+Lemma p2:
+  forall (D:Set) (a:D->Prop) (b:D->Prop),
+  (forall (x:D), a x) /\ (forall (x:D), b x)
+  -> (forall (x:D), a x /\ b x).
+  intros.
+  destruct H as [A B].
+  specialize (A x).
+  specialize (B x).
+  apply conj.
+  exact A.
+  exact B.
+Qed.
+
+
+Lemma p3:
+  forall (D:Set) (a:D->Prop) (b:D->Prop),
+  (forall (x:D), a x) \/ (forall (x:D), b x)
+  -> (forall (x:D), a x \/ b x).
+  intros.
+  destruct H.
+  specialize (H x).
+  left.
+  assumption.
+  specialize (H x).
+  right.
+  assumption.
+Qed.
+
